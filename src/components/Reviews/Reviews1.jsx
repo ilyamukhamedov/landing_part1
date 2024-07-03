c38daa664646fb66998cd7f2fdc8d014bc3fb705
Ilya <honeyflym@gmail.com>
2024-07-03 22:05:24 +0300

    fix commit issues

--- src/components/Reviews/Reviews1.jsx
+++ src/components/Reviews/Reviews1.jsx
@@ -1,17 +1,38 @@
 import React, { useRef, useState, useEffect } from "react";
 import "./Reviews1.css";
 import { reviewsData } from "../../utils/constants";
-import Swiper from "swiper";
+import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 
 const Reviews = () => {
+  const swiper = new Swiper(".card__content", {
+    loop: true,
+
+    pagination: {
+      el: ".swiper-pagination",
+    },
+
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
   return (
     <section className="container">
-      <div className="card__container">
+      <div className="card__container swiper">
         <div className="card__content">
-          <div>
+          <Swiper
+            spaceBetween={50}
+            slidesPerView={3}
+            onSlideChange={() => console.log("slide change")}
+            onSwiper={(swiper) => console.log(swiper)}
+            className="swiper-wrapper"
+          >
             {reviewsData.map((review) => (
-              <article className="card__article" key={review.id}>
+              <SwiperSlide
+                className="card__article swiper-slide"
+                key={review.id}
+              >
                 <div className="card__data">
                   <h2 className="card__name">{review.name}</h2>
 
@@ -19,10 +40,10 @@ const Reviews = () => {
 
                   <p className="card__description">{review.description}</p>
                 </div>
-              </article>
+              </SwiperSlide>
             ))}
             ;
-          </div>
+          </Swiper>
         </div>
       </div>
     </section>
