f4d8044350122089ad0b08d15a04ac018a7b7afa
Ilya <honeyflym@gmail.com>
2024-08-03 21:02:01 +0300

    delete files

--- src/utils/telegramApi.jsx
+++
@@ -1,3 +0,0 @@
-const TELEGRAM_BOT_TOKEN = import.meta.env.VITE_TELEGRAM_BOT_TOKEN;
-export const TELEGRAM_CHAT_ID = "@NB_Garden";
-export const API = `https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}/sendMessage`;
