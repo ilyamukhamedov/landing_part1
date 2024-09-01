46797ebb4e5976e784c2d9ef95f909373ab9b35f
Ilya <honeyflym@gmail.com>
2024-09-01 16:56:53 +0300

    check env settings

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   // base: "/book_1/",
+  base: import.meta.env.VITE_BASE_PATH || "/",
   server: {
     port: 5173,
   },
