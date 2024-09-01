c9e94da688c48971c6b5459d0be16062de7c1754
Ilya <honeyflym@gmail.com>
2024-09-01 17:08:06 +0300

    set vercel

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  // base: "/book_1/",
   base: import.meta.env.VITE_BASE_PATH || "/",
   server: {
     port: 5173,
