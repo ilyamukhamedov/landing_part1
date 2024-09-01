ee03a6fef1dea82f2a75fc914bc2005cf2350c00
Ilya <honeyflym@gmail.com>
2024-09-01 17:57:50 +0300

    set vercel

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,13 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig({
-  plugins: [react()],
-  base: import.meta.env.VITE_BASE_PATH || "/",
-  server: {
-    port: 5173,
-  },
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+  return {
+    plugins: [react()],
+    base: env.VITE_PATH || "/",
+    server: {
+      port: 5173,
+    },
+  };
 });
-console.log("hello");
